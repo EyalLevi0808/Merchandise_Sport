93e5cdb2da2161de1ee6556688a16ef284774be7
EyalLevi0808 <fceyal@gmail.com>
2021-09-16 23:59:42 +0300

    Add project files.

---
+++ Merchandise_Sport-master/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Merchandise_Sport_master.Models
+{
+    public class Product
+    {
+        [Key]
+        public int SerialNumber { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+
+        public int Size { get; set; }
+        [Required]
+        [Range(0, 600)]
+        [DataType(DataType.Currency)]
+
+        public int Price { get; set; }
+        [Required]
+
+        public string Image { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+    }
+}
---
+++ Merchandise_Sport-master/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Merchandise_Sport_master.Models
+{
+    public enum UserType
+    {
+        Client,
+        Admin,
+        Editor
+    }
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Username need to be more then one letter")]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Password need to be more then 4 letter")]
+        public string Password { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "email need to be more then 2 letter")]
+        public string Email { get; set; }
+
+
+        public string Address { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        
+        [Display(Name = "Favorite Team")]
+
+        public String FavoriteTeam { get; set; }
+
+        public UserType Type { get; set; } = UserType.Client;
+
+    }
+}
